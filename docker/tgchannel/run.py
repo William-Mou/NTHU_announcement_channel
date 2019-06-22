1708620313fea80784789ea6cf1a36a7c37362f6
William Mou <william.mou1024@gmail.com>
2019-06-22 16:26:03 +0800

    update .gitignore

--- docker/tgchannel/run.py
+++
@@ -1,6 +0,0 @@
-import os 
-for root, dirs, files in os.walk("./crawler"):
-    for f in files:
-        fullpath = os.path.join(root, f)
-        if fullpath[-3:] == ".py":
-            os.system("python " + fullpath)
--- todo_list.md
+++
@@ -1,3 +0,0 @@
-更改爬蟲路徑
-把程序 function 包起來
-[x] mysql link -> networks
